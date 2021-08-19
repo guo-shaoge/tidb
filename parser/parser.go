8e69cfb2598ac7e603f59c8678685e11b28a4867
wjHuang <huangwenjun1997@gmail.com>
2021-08-19 18:17:55 +0800

    [parser] fix select field again (#1317)

--- parser/parser.go
+++ parser/parser.go
@@ -17315,6 +17315,10 @@ yynewstate:
 	case 1648:
 		{
 			setOprList1 := yyS[yypt-3].item.([]ast.Node)
+			if sel, isSelect := setOprList1[len(setOprList1)-1].(*ast.SelectStmt); isSelect && !sel.IsInBraces {
+				endOffset := parser.endOffset(&yyS[yypt-2])
+				parser.setLastSelectFieldText(sel, endOffset)
+			}
 			var setOprList2 []ast.Node
 			var with2 *ast.WithClause
 			switch x := yyS[yypt-1].expr.(*ast.SubqueryExpr).Query.(type) {
@@ -17335,6 +17339,10 @@ yynewstate:
 	case 1649:
 		{
 			setOprList1 := yyS[yypt-3].item.([]ast.Node)
+			if sel, isSelect := setOprList1[len(setOprList1)-1].(*ast.SelectStmt); isSelect && !sel.IsInBraces {
+				endOffset := parser.endOffset(&yyS[yypt-2])
+				parser.setLastSelectFieldText(sel, endOffset)
+			}
 			var setOprList2 []ast.Node
 			var with2 *ast.WithClause
 			switch x := yyS[yypt-1].expr.(*ast.SubqueryExpr).Query.(type) {
@@ -17355,6 +17363,10 @@ yynewstate:
 	case 1650:
 		{
 			setOprList1 := yyS[yypt-4].item.([]ast.Node)
+			if sel, isSelect := setOprList1[len(setOprList1)-1].(*ast.SelectStmt); isSelect && !sel.IsInBraces {
+				endOffset := parser.endOffset(&yyS[yypt-3])
+				parser.setLastSelectFieldText(sel, endOffset)
+			}
 			var setOprList2 []ast.Node
 			var with2 *ast.WithClause
 			switch x := yyS[yypt-2].expr.(*ast.SubqueryExpr).Query.(type) {
