274a4e930cb9b6efa28e45acd7ed67775441e6d5
xhe <xw897002528@gmail.com>
2021-10-11 23:15:25 +0800

    parser: fix compiling, add unit tests (#28700)

--- parser/parser.go
+++ parser/parser.go
@@ -31,13 +31,13 @@ import __yyfmt__ "fmt"
 import (
 	"strings"
 
-	"github.com/pingcap/parser/ast"
-	"github.com/pingcap/parser/auth"
-	"github.com/pingcap/parser/charset"
-	"github.com/pingcap/parser/model"
-	"github.com/pingcap/parser/mysql"
-	"github.com/pingcap/parser/opcode"
-	"github.com/pingcap/parser/types"
+	"github.com/pingcap/tidb/parser/ast"
+	"github.com/pingcap/tidb/parser/auth"
+	"github.com/pingcap/tidb/parser/charset"
+	"github.com/pingcap/tidb/parser/model"
+	"github.com/pingcap/tidb/parser/mysql"
+	"github.com/pingcap/tidb/parser/opcode"
+	"github.com/pingcap/tidb/parser/types"
 )
 
 type yySymType struct {
